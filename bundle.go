25d06ba514b6ef6ed7c5028c00e5b892e3001961
Michael Stapelberg <michael@stapelberg.de>
2019-10-19 22:41:59 +0200

    gofmt GENERATED_bundled.go

--- bundle.go
+++ bundle.go
@@ -1,3 +1,3 @@
 package gokrazy
 
-//go:generate sh -c "go run goembed.go -package bundled -var assets assets/header.tmpl assets/footer.tmpl assets/overview.tmpl assets/status.tmpl assets/favicon.ico assets/bootstrap-3.3.7.min.css assets/bootstrap-table-1.11.0.min.css assets/bootstrap-table-1.11.0.min.js assets/jquery-3.1.1.min.js > internal/bundled/GENERATED_bundled.go"
+//go:generate sh -c "go run goembed.go -package bundled -var assets assets/header.tmpl assets/footer.tmpl assets/overview.tmpl assets/status.tmpl assets/favicon.ico assets/bootstrap-3.3.7.min.css assets/bootstrap-table-1.11.0.min.css assets/bootstrap-table-1.11.0.min.js assets/jquery-3.1.1.min.js > internal/bundled/GENERATED_bundled.go && gofmt -w internal/bundled/GENERATED_bundled.go"
