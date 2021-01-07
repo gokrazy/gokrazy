bdcac6933ac1495a8e7be5122e5a754e8dd55002
andig <cpuidle@gmx.de>
2021-01-07 07:46:47 +0100

    Don't redirect when localhost

--- httpsredirect.go
+++ httpsredirect.go
@@ -1,10 +1,27 @@
 package gokrazy
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 // TODO: Configurable https-port
-func httpsRedirect(w http.ResponseWriter, r *http.Request) {
-	r.URL.Host = r.Host
-	r.URL.Scheme = "https"
-	http.Redirect(w, r, r.URL.String(), http.StatusFound) // Redirect to https
+func httpsRedirect(handler http.Handler) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// get hostname without port
+		target := r.URL.Host
+		if port := ":" + r.URL.Port(); strings.HasSuffix(target, port) {
+			target = strings.TrimSuffix(target, port)
+		}
+
+		// serve localhost on http
+		if target == "localhost" {
+			handler.ServeHTTP(w, r)
+			return
+		}
+
+		r.URL.Host = r.Host
+		r.URL.Scheme = "https"
+		http.Redirect(w, r, r.URL.String(), http.StatusFound) // Redirect to https
+	}
 }
