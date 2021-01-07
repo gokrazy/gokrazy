97987794f9c7ad5dd1c5c56d56eb618c8ecfb498
andig <cpuidle@gmx.de>
2021-01-07 16:49:46 +0100

    Don't redirect requests from localhost to HTTPS (#78)

--- httpsredirect.go
+++ httpsredirect.go
@@ -1,9 +1,18 @@
 package gokrazy
 
-import "net/http"
+import (
+	"net"
+	"net/http"
+)
 
-// TODO: Configurable https-port
 func httpsRedirect(w http.ResponseWriter, r *http.Request) {
+	host, _, _ := net.SplitHostPort(r.RemoteAddr)
+	ip := net.ParseIP(host)
+	if ip.IsLoopback() {
+		http.DefaultServeMux.ServeHTTP(w, r)
+		return
+	}
+
 	r.URL.Host = r.Host
 	r.URL.Scheme = "https"
 	http.Redirect(w, r, r.URL.String(), http.StatusFound) // Redirect to https
