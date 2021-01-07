45a49ce923c9b0d6a5033235370f7372dccea48f
andig <cpuidle@gmx.de>
2021-01-07 10:16:40 +0100

    Split host/port

--- httpsredirect.go
+++ httpsredirect.go
@@ -7,7 +7,12 @@ import (
 
 func httpsRedirect(w http.ResponseWriter, r *http.Request) {
 	// get hostname without port
-	ip := net.ParseIP(r.RemoteAddr)
+	host, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		host = r.RemoteAddr
+	}
+
+	ip := net.ParseIP(host)
 	if IsInPrivateNet(ip) {
 		http.DefaultServeMux.ServeHTTP(w, r)
 		return
