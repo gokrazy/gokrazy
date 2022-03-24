908dfc55333af91d7cda3fa36f309b107f32afb4
oliverpool <git@olivier.pfad.fr>
2022-03-24 17:27:37 +0100

    authenticated: use strings.Cut

--- authenticated.go
+++ authenticated.go
@@ -13,24 +13,24 @@ func authenticated(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "httpPassword not set", http.StatusInternalServerError)
 		return
 	}
-	s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
-	if len(s) != 2 || s[0] != "Basic" {
+	kind, encoded, found := strings.Cut(r.Header.Get("Authorization"), " ")
+	if !found || kind != "Basic" {
 		w.Header().Set("WWW-Authenticate", `Basic realm="gokrazy"`)
 		http.Error(w, "no Basic Authorization header set", http.StatusUnauthorized)
 		return
 	}
 
-	b, err := base64.StdEncoding.DecodeString(s[1])
+	b, err := base64.StdEncoding.DecodeString(encoded)
 	if err != nil {
 		w.Header().Set("WWW-Authenticate", `Basic realm="gokrazy"`)
 		http.Error(w, fmt.Sprintf("could not decode Authorization header as base64: %v", err), http.StatusUnauthorized)
 		return
 	}
 
-	pair := strings.SplitN(string(b), ":", 2)
-	if len(pair) != 2 ||
-		pair[0] != "gokrazy" ||
-		pair[1] != httpPassword {
+	username, password, found := strings.Cut(string(b), ":")
+	if !found ||
+		username != "gokrazy" ||
+		password != httpPassword {
 		w.Header().Set("WWW-Authenticate", `Basic realm="gokrazy"`)
 		http.Error(w, "invalid username/password", http.StatusUnauthorized)
 		return
