e950d9699a56a56edc2a5a79167333090c97d89c
Michael Stapelberg <michael@stapelberg.de>
2022-08-07 10:46:11 +0200

    remove bundled strings.Cut in favor of standard library
    (now that Go 1.18 is our minimum version)

--- authenticated.go
+++ authenticated.go
@@ -14,7 +14,7 @@ func authenticated(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "httpPassword not set", http.StatusInternalServerError)
 		return
 	}
-	kind, encoded, found := stringsCut(r.Header.Get("Authorization"), " ")
+	kind, encoded, found := strings.Cut(r.Header.Get("Authorization"), " ")
 	if !found || kind != "Basic" {
 		w.Header().Set("WWW-Authenticate", `Basic realm="gokrazy"`)
 		http.Error(w, "no Basic Authorization header set", http.StatusUnauthorized)
@@ -28,7 +28,7 @@ func authenticated(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	username, password, found := stringsCut(string(b), ":")
+	username, password, found := strings.Cut(string(b), ":")
 	if !found ||
 		username != "gokrazy" ||
 		subtle.ConstantTimeCompare([]byte(password), []byte(httpPassword)) != 1 {
@@ -38,22 +38,4 @@ func authenticated(w http.ResponseWriter, r *http.Request) {
 	}
 
 	http.DefaultServeMux.ServeHTTP(w, r)
-}
-
-// TODO: replace with strings.Cut (available in go 1.18)
-// when go 1.19 is out
-// https://github.com/gokrazy/gokrazy/pull/120#discussion_r834514081
-//
-// Cut slices s around the first instance of sep,
-// returning the text before and after sep.
-// The found result reports whether sep appears in s.
-// If sep does not appear in s, cut returns s, "", false.
-//
-// Copyright 2009 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-func stringsCut(s, sep string) (before, after string, found bool) {
-	if i := strings.Index(s, sep); i >= 0 {
-		return s[:i], s[i+len(sep):], true
-	}
-	return s, "", false
 }
